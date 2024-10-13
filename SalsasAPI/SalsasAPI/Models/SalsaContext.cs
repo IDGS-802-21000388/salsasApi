ef667839d1039982d000b3838e4d9ba8bb8fad77
EdwinY-a <21000445@alumnos.utleon.edu.mx>
2024-10-13 16:07:41 -0600

    Cambios

--- SalsasAPI/SalsasAPI/Models/SalsaContext.cs
+++ SalsasAPI/SalsasAPI/Models/SalsaContext.cs
@@ -74,6 +74,8 @@ public partial class SalsaContext : DbContext
 
     public virtual DbSet<InventarioReporte> InventarioReporte { get; set; }
     public virtual DbSet<RankingClientes> RankingClientes { get; set; }
+
+    public virtual DbSet<AgentesVenta> AgentesVenta { get; set; }
     
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
